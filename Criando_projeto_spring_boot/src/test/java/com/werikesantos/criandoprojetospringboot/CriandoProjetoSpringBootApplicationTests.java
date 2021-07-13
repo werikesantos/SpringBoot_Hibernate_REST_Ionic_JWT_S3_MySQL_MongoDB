ca782086b16c0426dcfa35a34813156bbf9308ea
werikesantos <werike.ms@gmail.com>
2021-07-13 15:05:50 -0300

    Projeto Spring Boot criado

---
+++ Criando_projeto_spring_boot/src/test/java/com/werikesantos/criandoprojetospringboot/CriandoProjetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.werikesantos.criandoprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriandoProjetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
